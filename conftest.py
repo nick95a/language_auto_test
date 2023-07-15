f010a2326551ee210d49a5770e9fb7781685a6fd
nick95a <nick95a@yandex.ru>
2023-07-15 21:59:44 +0500

    add pytest.ini

--- conftest.py
+++ conftest.py
@@ -5,13 +5,13 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
 
-language = input("Enter the language")
+language = 'es'
 
-@pytest.fixture(scope = "class")
+@pytest.fixture(scope = 'function')
 def browser(language):
+    
     browser = webdriver.Chrome()
-    link = f"http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/{language}/"
-    browser.get(link)
+    browser.get(f"http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/{language}/")
     yield browser
     browser.quit()
     
--- test_items.py
+++ test_items.py
@@ -4,10 +4,11 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
+
 @pytest.mark.parametrize('language', ["es", "fr"])
-def test_add_to_cart_button(browser):
+def test_add_to_cart_button(browser, language):
     
-    browser.get("http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/")
+    browser.get(f"http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/{language}/")
     time.sleep(30)
     add_button = WebDriverWait(browser, 5).until(EC.element_to_be_clickable((By.CSS_SELECTOR, "button.btn-add-to-basket")))
     assert add_button, "The button is not available"
