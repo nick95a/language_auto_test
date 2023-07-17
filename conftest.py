d7b0af04c2fcf0782d088d9a3e51013681e37c9a
nick95a <nick95a@yandex.ru>
2023-07-17 21:51:35 +0500

    Added command line reader and browser configuration

--- conftest.py
+++ conftest.py
@@ -3,15 +3,48 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.chrome.options import Options
 
 
+"""
 language = 'es'
+options.add_experimental_option('prefs', {'intl.accept_languages': language})
+"""
+
+options = Options()
+link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
+
+def pytest_addoption(parser):
+    parser.addoption('--browser_name', action = 'store', default = 'chrome',
+                      help = "Input your browser")
+    parser.addoption('--language', action = 'store', default = 'es', 
+                     help = 'Input your language')
 
 @pytest.fixture(scope = 'function')
-def browser(language):
+def browser(request):
     
-    browser = webdriver.Chrome()
-    browser.get(f"http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/{language}/")
+    browser_name = request.config.getoption('browser_name')
+    browser_language = request.config.getoption('language')
+    browser = None
+    
+    if browser_name == 'chrome':
+        browser = webdriver.Chrome()
+    else:
+        browser = None
+        raise Exception("Browser should be chrome")
+    
+    if browser_language == 'es':
+        """es_link = link + browser_language + "/"
+        browser.get(es_link)
+        """
+        options.add_experimental_option('prefs', {'intl.accept_languages': browser_language})
+    elif browser_language == 'fr':
+        fr_link = link + browser_language + "/"
+        browser.get(fr_link)
+        options.add_experimental_option('prefs', {'intl.accept_languages': browser_language})
+    else:
+        browser.get(link)
+
     yield browser
     browser.quit()
     
--- test_items.py
+++ test_items.py
@@ -5,10 +5,10 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
 
-@pytest.mark.parametrize('language', ["es", "fr"])
-def test_add_to_cart_button(browser, language):
-    
-    browser.get(f"http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/{language}/")
-    time.sleep(30)
+@pytest.mark.parametrize('language', ["es", "fr", "en"])
+@pytest.mark.parametrize('browser_name', ["chrome", "firefox"])
+def test_add_to_cart_button(browser):
+
+    time.sleep(10)
     add_button = WebDriverWait(browser, 5).until(EC.element_to_be_clickable((By.CSS_SELECTOR, "button.btn-add-to-basket")))
     assert add_button, "The button is not available"
